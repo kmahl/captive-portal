b15dac1237ffb6a565f87d26d69baa569e40edab
kmahl <carlossavch@gmail.com>
2024-11-12 23:01:55 -0300

    asd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <SiteProvider>
         <Router>
           <CssBaseline />
-          <Container maxWidth="sm" style={{ padding: "0", minHeight: "100vh", display: "flex", alignItems: "center", justifyContent: "center", fontFamily: 'Rubik' }}>
+          <Container maxWidth="sm" style={{ padding: "0", minHeight: "80vh", display: "flex", alignItems: "center", justifyContent: "center", fontFamily: 'Rubik' }}>
             <Routes>
               <Route
                 path="/guest/s/:site"
